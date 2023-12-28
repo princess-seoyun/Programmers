d8b7f88bb14b8a36d9ebab0d9c1614a1982f35ed
seoyunZZang <107830173+princess-seoyun@users.noreply.github.com>
2023-12-28 21:02:07 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.11 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        String[] arr = String.valueOf(n).split("");
+        
+        for(int i = 0; i < arr.length ; i++)
+        {
+            answer += Integer.parseInt(arr[i]);
+        }
+
+        return answer;
+    }
+}
