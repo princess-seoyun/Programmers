1fb07d8e8165b9964a34e4ca5c3a587668c2246a
seoyunZZang <107830173+princess-seoyun@users.noreply.github.com>
2023-12-29 17:25:23 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i = 0 ; i < signs.length ; i++){
+            if(signs[i] == false)
+            {
+                absolutes[i] = -absolutes[i];
+            }
+        }
+        
+        for(int i = 0 ; i < absolutes.length ; i++) {
+            answer += absolutes[i];
+        }
+        
+        
+        return answer;
+    }
+}
