5494d8e3a493a67eef42ea26288b0eec937de4bc
seoyunZZang <107830173+princess-seoyun@users.noreply.github.com>
2024-07-29 23:48:19 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution(_ angle:Int) -> Int {
+    
+    var answer : Int = 0
+    
+    if angle < 90 && angle > 0 {
+        return 1
+    } else if angle == 90 {
+        return 2
+    } else if angle > 90 && angle < 180 {
+        return 3
+    } else if angle == 180 {
+        return 4
+    }
+    
+    return 0
+}
