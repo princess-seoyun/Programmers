5b887a9504829724835637d358e8d5e9d474e629
seoyunZZang <107830173+princess-seoyun@users.noreply.github.com>
2024-08-25 18:40:39 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.04 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ num_list:[Int], _ n:Int) -> [Int] {
+    
+    var answer : [Int] = []
+    
+    for i in 0..<n {
+        answer.append(num_list[i])
+    }
+    
+    return answer
+}
