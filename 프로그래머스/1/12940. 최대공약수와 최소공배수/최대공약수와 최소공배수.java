d575cc8267e6041388a6907be0bc818c7dc01c48
seoyunZZang <107830173+princess-seoyun@users.noreply.github.com>
2026-06-01 16:30:44 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 64.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int m) {
+        
+        // 정답을 담을 배열
+        int[] answer = new int[2];
+        
+        // 두 수의 최대공약수
+        int gcdVal = gcd(n, m);
+        
+        // 두 수의 최소공배수
+        int lcmVal = (n * m) / gcdVal; 
+        
+        answer[0] = gcdVal;
+        answer[1] = lcmVal;
+        
+        return answer;
+    }
+    
+    // 재귀함수
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+}
+
+
+
